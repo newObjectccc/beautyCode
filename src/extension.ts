d0862067727b26ce0cf1a755dfdac81e801483de
newObjectccc <897333949@qq.com>
2024-03-07 00:44:31 +0800

    feat: get text line

--- src/extension.ts
+++ src/extension.ts
@@ -5,4 +5,9 @@ export function activate() {
     vscode.message.info('Hello, World!');
     console.log('Hello, World!');
   });
+  vscode.registerCommand('extension.smartConsole', () => {
+    const selectInfo = vscode.getSelection();
+    const curLanguageId = vscode.getActiveTextEditorLanguageId();
+    console.log(selectInfo, curLanguageId, '111');
+  });
 }
