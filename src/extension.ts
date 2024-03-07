0d756bbc52e7be61698020a1a7e15ac249aa66f8
newObjectccc <897333949@qq.com>
2024-03-07 18:29:10 +0800

    feat: command

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,8 @@
 import * as vscode from '@vscode-use/utils';
 
 export function activate() {
-  vscode.registerCommand('extension.helloWorld', () => {
-    vscode.message.info('Hello, World!');
-    console.log('Hello, World!');
-  });
-  vscode.registerCommand('extension.smartConsole', () => {
-    const selectInfo = vscode.getSelection();
-    const curLanguageId = vscode.getActiveTextEditorLanguageId();
-    console.log(selectInfo, curLanguageId, '111');
+  vscode.registerCommand('extension.beautyCode', () => {
+    const selection = vscode.getSelection();
+    console.log('selection', selection);
   });
 }
